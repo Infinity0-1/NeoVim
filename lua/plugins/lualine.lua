111ebcbe251c0bcfb8cc9dc1cc0f8073107b001e
Infinity <abdelkader.belhadj@enstsa.edu.dz>
2026-03-29 10:07:36 +0100

    Updated personal configurations

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -35,7 +35,7 @@ return {
 
       require("lualine").setup({
         options = {
-          theme = "catppuccin",
+          theme = "catppuccin-nvim",
           icons_enabled = true,
           component_separators = { left = '', right = ''},
           section_separators = { left = '', right = ''},
